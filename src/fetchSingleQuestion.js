953812efce6bbda79e0bf7efc417400134c82d17
Erin Wolff <erin_wolff@outlook.com>
2024-02-12 19:09:57 -0800

    created separate file for fetchSingleQuestion function

---
+++ src/fetchSingleQuestion.js
@@ -0,0 +1,23 @@
+const shuffleArray = require('./shuffleArray');
+
+// Function to fetch a single question when a specific category is chosen
+module.exports = async function fetchSingleQuestion(category) {
+  try {
+    const fetch = (await import('node-fetch')).default;
+    let url = `https://the-trivia-api.com/v2/questions?limit=1&categories=${category}`; // Fetch a single question
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const questionItem = data[0];
+
+    return { // Return a formatted question object 
+      question: questionItem.question.text,
+      category: questionItem.category,
+      allAnswers: shuffleArray([questionItem.correctAnswer, ...questionItem.incorrectAnswers]),
+      correctAnswer: questionItem.correctAnswer
+    };
+  } catch (error) {
+    console.error('Error fetching single question:', error);
+  }
+}
