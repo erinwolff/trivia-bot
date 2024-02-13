c688bc9c3eacd54a12b467895b70ef04d56517fa
Erin Wolff <erin_wolff@outlook.com>
2024-02-12 19:10:13 -0800

    function to fetch a single randomly categorized question

---
+++ src/fetchRandomQuestions.js
@@ -0,0 +1,20 @@
+const shuffleArray = require('./shuffleArray');
+
+// Function to fetch questions to be stored in the questionBank & used for Random/"Surprise Me!" button selection
+module.exports = async function fetchRandomQuestions() {
+  try {
+    const fetch = (await import('node-fetch')).default;
+    let url = 'https://the-trivia-api.com/v2/questions?limit=20'; // Can update batch limit, up to 50 questions.
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data.map(questionItem => ({
+      question: questionItem.question.text,
+      category: questionItem.category,
+      allAnswers: shuffleArray([questionItem.correctAnswer, ...questionItem.incorrectAnswers]),
+      correctAnswer: questionItem.correctAnswer
+    }));
+  } catch (error) {
+    console.error('Error fetching question batch:', error);
+  }
+};
