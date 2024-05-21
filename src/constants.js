bce3e32f232e7ccd623128466483556f8456eaaa
Erin Wolff <erin_wolff@outlook.com>
2024-05-21 13:41:11 -0700

    constants created

---
+++ src/constants.js
@@ -0,0 +1,31 @@
+const sassyMessages = [
+  "Oops, it's not your turn!",
+  "Hey, show a little patience!",
+  "Are you seriously trying again?",
+  "What is wrong with you?",
+  "You're about as competent as a screen door on a submarine, and your grasp on reality is only rivaled by your ability to grasp coherent thought—pathetically nonexistent",
+  "You possess the intellectual depth of a shallow puddle in a drought. It's almost an achievement in itself — if only it weren't so profoundly uninspiring.",
+  "If impatience were a virtue, you'd be the patron saint of instant gratification. It's almost impressive how little you seem to understand the concept of waiting—it's like watching a toddler trying to play 4D chess.",
+  "You are a living, breathing stain on the fabric of humanity, a festering boil on the ass of progress.",
+  "Your very existence is a testament to the failures of nature and the utter incompetence of your parents.",
+  "A SWAT team is on the way to your location.",
+];
+
+// API Category Query Parameters
+const allCategories = [
+  "Music",
+  "Sport_and_Leisure",
+  "Film_and_TV",
+  "Arts_and_Literature",
+  "History",
+  "Society_and_Culture",
+  "Science",
+  "Geography",
+  "Food_and_Drink",
+  "General_Knowledge",
+];
+
+module.exports = {
+  allCategories,
+  sassyMessages,
+};
