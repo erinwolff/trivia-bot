8dd4473e62c2318821a69d3e1f61b0f6d5e1004c
Erin Wolff <erin_wolff@outlook.com>
2024-06-06 09:43:08 -0700

    modified to shorten answer strings when longer than 80 chars

--- src/fetchRandomQuestions.js
+++ src/fetchRandomQuestions.js
@@ -1,5 +1,13 @@
 const shuffleArray = require("./shuffleArray");
 
+// Function to format answer when it is greater than 80 characters
+function formatAnswer(answer, maxLength = 80) {
+  if (answer.length > maxLength) {
+    return answer.slice(0, maxLength - 3) + "...";
+  }
+  return answer;
+}
+
 // Function to fetch questions to be stored in the questionBank & used for Random/"Surprise Me!" button selection
 module.exports = async function fetchRandomQuestions() {
   try {
@@ -12,10 +20,17 @@ module.exports = async function fetchRandomQuestions() {
       question: questionItem.question.text,
       category: questionItem.category,
       allAnswers: shuffleArray([
-        questionItem.correctAnswer,
-        ...questionItem.incorrectAnswers,
+        questionItem.correctAnswer.length > 80
+          ? formatAnswer(questionItem.correctAnswer)
+          : questionItem.correctAnswer,
+        ...questionItem.incorrectAnswers.map((answer) =>
+          answer.length > 80 ? formatAnswer(answer) : answer
+        ),
       ]),
-      correctAnswer: questionItem.correctAnswer,
+      correctAnswer:
+        questionItem.correctAnswer.length > 80
+          ? formatAnswer(questionItem.correctAnswer)
+          : questionItem.correctAnswer,
     }));
   } catch (error) {
     console.error("Error fetching question batch:", error);
--- src/fetchSingleQuestion.js
+++ src/fetchSingleQuestion.js
@@ -1,5 +1,13 @@
 const shuffleArray = require("./shuffleArray");
 
+// Function to format answer when it is greater than 80 characters
+function formatAnswer(answer, maxLength = 80) {
+  if (answer.length > maxLength) {
+    return answer.slice(0, maxLength - 3) + "...";
+  }
+  return answer;
+}
+
 // Function to fetch a single question when a specific category is chosen
 module.exports = async function fetchSingleQuestion(category) {
   try {
@@ -16,10 +24,17 @@ module.exports = async function fetchSingleQuestion(category) {
       question: questionItem.question.text,
       category: questionItem.category,
       allAnswers: shuffleArray([
-        questionItem.correctAnswer,
-        ...questionItem.incorrectAnswers,
+        questionItem.correctAnswer.length > 80
+          ? formatAnswer(questionItem.correctAnswer)
+          : questionItem.correctAnswer,
+        ...questionItem.incorrectAnswers.map((answer) =>
+          answer.length > 80 ? formatAnswer(answer) : answer
+        ),
       ]),
-      correctAnswer: questionItem.correctAnswer,
+      correctAnswer:
+        questionItem.correctAnswer.length > 80
+          ? formatAnswer(questionItem.correctAnswer)
+          : questionItem.correctAnswer,
     };
   } catch (error) {
     console.error("Error fetching single question:", error);
